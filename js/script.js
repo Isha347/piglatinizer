d36257506faee1a397e876189b6435c472351f87
Isha347 <awong@cristoreyny.org>
2019-11-20 22:50:21 +0000

    updated  javascript and html

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
+$("button").click(function(){
 
+let userInput =$("input").val();
+let split = userInput.split(" ");
+//write an if function
 
-
-  
-  
-  
-  
+});
+    
 });
